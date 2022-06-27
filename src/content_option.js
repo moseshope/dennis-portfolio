ec1af3eed69fb3a28faa6f362bc3baff2054cf48
dennisYun12 <hello.dennis0102@gmail.com>
2022-06-27 11:00:24 +0900

    First commit of my site

--- src/content_option.js
+++ src/content_option.js
@@ -1,38 +1,40 @@
-const logotext = "JOHN";
+const logotext = "Dennis.Y";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    title: "Dennis.Y",
+    description: "I’m Dennis Yun - Senior Web developer, currently working remotely",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "I’m Dennis Yun",
     animated: {
         first: "I love coding",
         second: "I code cool websites",
-        third: "I develop mobile apps",
+        third: "I develop web apps",
     },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    description: "Innovative, task-driven professional with 8+ years of experience in web design and development across diverse industries." +  
+                 "nEquipped with a recode of success in consistently identifying and providing the technological needs of companies through ingenious innovation."+
+                 "Proficient in developing databases, creating user interfaces, writing and testing codes, troubleshooting simple/complex issues, and implementing new features based on user feedback.",
     your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
 };
 
 const dataabout = {
-    title: "abit about my self",
-    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+    title: "What is my goal",
+    aboutme: "What is important to me is always meeting the needs of the other party and taking responsibility for the project from launch to success. Code quality is the heart of a developer. At the end of each work day, I like receiving feedback from client at regular intervals.",
 };
 const worktimeline = [{
-        jobtitle: "Designer of week",
-        where: "YAdfi",
-        date: "2020",
+        jobtitle: "Weekdays",
+        where: "Remote",
+        date: "09:00' ~ 17:00'",
     },
     {
-        jobtitle: "Designer of week",
-        where: "Jamalya",
-        date: "2019",
+        jobtitle: "Interview",
+        where: "Video call",
+        date: "Every 18:00' except weekend",
     },
     {
-        jobtitle: "Designer of week",
-        where: "ALquds",
-        date: "2019",
+        jobtitle: "Beak days",
+        where: "holiday & Weekend",
+        date: "No working",
     },
 ];
 
@@ -45,30 +47,74 @@ const skills = [{
         value: 85,
     },
     {
-        name: "Javascript",
-        value: 80,
+        name: "PHP",
+        value: 100,
+    },
+    {
+        name: "JavaScript",
+        value: 100,
+    },
+    {
+        name: "TypeScript",
+        value: 100,
     },
     {
         name: "React",
-        value: 60,
+        value: 100,
+    },
+    {
+        name: "Angular",
+        value: 98,
     },
     {
         name: "Jquery",
-        value: 85,
+        value: 100,
+    },
+    {
+        name: "Vue.js",
+        value: 100,
+    },
+    {
+        name: "MySQL",
+        value: 100,
+    },
+    {
+        name: "PostgreSQL",
+        value: 100,
+    },
+    {
+        name: "Github",
+        value: 100,
+    },
+    {
+        name: "Jirra",
+        value: 100,
+    },
+    {
+        name: "Trello",
+        value: 100,
+    },
+    {
+        name: "Laravel",
+        value: 95,
+    },
+    {
+        name: "Shopify",
+        value: 95,
     },
 ];
 
 const services = [{
         title: "UI & UX Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        description: "I can provide creative UI/UX design.",
     },
     {
-        title: "Mobile Apps",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        title: "Web app",
+        description: "Can develop perfect web application every on time you want.",
     },
     {
-        title: "Wordpress Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        title: "E-Commerce marketplace",
+        description: "Multi fuctional marketpalce can be developed with senior business logic and best quality of code.",
     },
 ];
 
@@ -108,37 +154,12 @@ const dataportfolio = [{
         desctiption: "The wisdom of life consists in the elimination of non-essentials.",
         link: "#",
     },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/550/?grayscale",
-        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "name@domain.com",
-    YOUR_FONE: "(555)123-4567",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    YOUR_EMAIL: "hello.dennis0102@gmail.com",
+    YOUR_FONE: "+1 905-282-6789",
+    description: "You can send at any time you want on work days",
     YOUR_SERVICE_ID: "service_id",
     YOUR_TEMPLATE_ID: "template_id",
     YOUR_USER_ID: "user_id",
